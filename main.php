dd2b77a6a5102af0e8418e6f40734f363228ec9b
Jorma Kõpp <jormkop@greeny.cs.tlu.ee>
2015-11-25 11:45:17 +0200

    done deal

--- main.php
+++ main.php
@@ -1,3 +1,111 @@
 <?php require_once("header.php"); ?>
-    <h1>Hello, world!</h1>
+
+
+	<!-- ##################### -->
+	<!-- ###-----MENÜÜ-----### -->
+	<!-- ##################### -->
+
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+	<br><br><br><br>
+	
+	
+	
+	
+	<!-- ##################### -->
+	<!-- ###-----SISU------### -->
+	<!-- ##################### -->
+	
+
+	
+<div class="container">
+
+	<div class="row">
+		<div class="col-sm-6 col-md-push-6">
+	<form>
+	  <form >
+		  <div class="form-group">
+			<label for="inputEmail3" class="col-sm-2 control-label"></label>
+			<div class="col-sm-10">
+			  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+			</div>
+		  </div>
+		  <div class="form-group">
+			<label for="inputPassword3" class="col-sm-2 control-label"></label>
+			<div class="col-sm-10">
+			  <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+			</div>
+			<br>
+		  </div>
+		  <div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+			  <button type="submit" class="btn btn-success">Sign in</button>
+			<br>
+			</div>
+		  </div>
+		</form>
+		  <form >
+		   <div class="form-group">
+			<label for="inputEmail3" class="col-sm-2 control-label"></label>
+			<div class="col-sm-10">
+			  <input type="email" class="form-control" id="inputEmail3" placeholder="Full name">
+			</div>
+		  </div>
+		  <div class="form-group">
+			<label for="inputEmail3" class="col-sm-2 control-label"></label>
+			<div class="col-sm-10">
+			  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+			</div>
+		  </div>
+		  <div class="form-group">
+			<label for="inputPassword3" class="col-sm-2 control-label"></label>
+			<div class="col-sm-10">
+			  <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+			</div>
+			<br>
+		  </div>
+		  <div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+			  <button type="submit" class="btn btn-success">Submit</button>
+			</div>
+		  </div>
+		</form>
+	</form>
+		</div>
+		<div class="col-sm-6 col-md-pull-6">
+			<div class="jumbotron">
+				<div class="container">
+				  <h1>Welcome to Twitter.</h1>
+				  <p>Connect with your friends — and other fascinating people. Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.</p>
+				 </div>
+			</div>
+		</div>
+		
+	</div>
+</div>
+
+
+
 <?php require_once("footer.php"); ?>
